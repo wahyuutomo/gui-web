5b29d0e7877934b095ac2ebd56fc8711cafc010a
wahyuutomo <wahyuutomo@users.noreply.github.com>
2014-05-20 21:45:59 +0700

    Update update-lamp.php checkbox
    checkbox sukses ditambahkan

--- update-lamp.php
+++ update-lamp.php
@@ -1,58 +1,57 @@
 <?php
-	header('Content-Type: application/json');
+//	header('Content-Type: application/json');
 	//$jam_on = $_POST['jam'];
 	//*
-    $jamon0 = $_POST['lampu[0].jamon'];
-    $jamon1 = $_POST['lampu[1].jamon'];
-    $jamon2 = $_POST['lampu[2].jamon'];
-    $jamon3 = $_POST['lampu[3].jamon'];
-    $jamon4 = $_POST['lampu[4].jamon'];
-    $jamon5 = $_POST['lampu[5].jamon'];
-    $jamon6 = $_POST['lampu[6].jamon'];
-    $jamon7 = $_POST['lampu[7].jamon'];
-    
-    $jamoff0 = $_POST['lampu[0].jamoff'];
-    $jamoff1 = $_POST['lampu[1].jamoff'];
-    $jamoff2 = $_POST['lampu[2].jamoff'];
-    $jamoff3 = $_POST['lampu[3].jamoff'];
-    $jamoff4 = $_POST['lampu[4].jamoff'];
-    $jamoff5 = $_POST['lampu[5].jamoff'];
-    $jamoff6 = $_POST['lampu[6].jamoff'];
-    $jamoff7 = $_POST['lampu[7].jamoff'];
-    
-    $meniton0 = $_POST['lampu[0].meniton'];
-    $meniton1 = $_POST['lampu[1].meniton'];
-    $meniton2 = $_POST['lampu[2].meniton'];
-    $meniton3 = $_POST['lampu[3].meniton'];
-    $meniton4 = $_POST['lampu[4].meniton'];
-    $meniton5 = $_POST['lampu[5].meniton'];
-    $meniton6 = $_POST['lampu[6].meniton'];
-    $meniton7 = $_POST['lampu[7].meniton'];
-    
-    $menitoff0 = $_POST['lampu[0].menitoff'];
-	$menitoff1 = $_POST['lampu[1].menitoff'];
-    $menitoff2 = $_POST['lampu[2].menitoff'];
-	$menitoff3 = $_POST['lampu[3].menitoff'];
-	$menitoff4 = $_POST['lampu[4].menitoff'];
-	$menitoff5 = $_POST['lampu[5].menitoff'];
-	$menitoff6 = $_POST['lampu[6].menitoff'];
-	$menitoff7 = $_POST['lampu[7].menitoff'];
+//*
+    $jamon0 = $_POST['jamon0'];
+    $jamon1 = $_POST['jamon1'];
+    $jamon2 = $_POST['jamon2'];
+    $jamon3 = $_POST['jamon3'];
+    $jamon4 = $_POST['jamon4'];
+    $jamon5 = $_POST['jamon5'];
+    $jamon6 = $_POST['jamon6'];
+    $jamon7 = $_POST['jamon7'];
+    $jamoff0 = $_POST['jamoff0'];
+    $jamoff1 = $_POST['jamoff1'];
+    $jamoff2 = $_POST['jamoff2'];
+    $jamoff3 = $_POST['jamoff3'];
+    $jamoff4 = $_POST['jamoff4'];
+    $jamoff5 = $_POST['jamoff5'];
+    $jamoff6 = $_POST['jamoff6'];
+    $jamoff7 = $_POST['jamoff7'];
+    $meniton0 = $_POST['meniton0'];
+    $meniton1 = $_POST['meniton1'];
+    $meniton2 = $_POST['meniton2'];
+    $meniton3 = $_POST['meniton3'];
+    $meniton4 = $_POST['meniton4'];
+    $meniton5 = $_POST['meniton5'];
+    $meniton6 = $_POST['meniton6'];
+    $meniton7 = $_POST['meniton7'];
+    $menitoff0 = $_POST['menitoff0'];
+	$menitoff1 = $_POST['menitoff1'];
+    $menitoff2 = $_POST['menitoff2'];
+	$menitoff3 = $_POST['menitoff3'];
+	$menitoff4 = $_POST['menitoff4'];
+	$menitoff5 = $_POST['menitoff5'];
+	$menitoff6 = $_POST['menitoff6'];
+	$menitoff7 = $_POST['menitoff7'];
+	//*/
 	
-	$auto0 = $_POST['lampu[0].auto'];
-	$auto1 = $_POST['lampu[1].auto'];
-	$auto2 = $_POST['lampu[2].auto'];
-	$auto3 = $_POST['lampu[3].auto'];
-	$auto4 = $_POST['lampu[4].auto'];
-	$auto5 = $_POST['lampu[5].auto'];
-	$auto6 = $_POST['lampu[6].auto'];
-	$auto7 = $_POST['lampu[7].auto'];
+	$auto0 = $_POST['auto0'];
+	$auto1 = $_POST['auto1'];
+	$auto2 = $_POST['auto2'];
+	$auto3 = $_POST['auto3'];
+	$auto4 = $_POST['auto4'];
+	$auto5 = $_POST['auto5'];
+	$auto6 = $_POST['auto6'];
+	$auto7 = $_POST['auto7'];
 	
 	//*/
     
     $koneksi=mysql_connect("localhost", "root", "1214174");
     if ($koneksi){mysql_select_db("monitoring");}
   //  $sql=mysql_query("update lampu set jam_on='$jam_on', jam_off='$jam_off',menit_on='$menit_on', menit_off='$menit_off' WHERE id = 0");         
- /*
+ //*
     $sql=mysql_query("update lampu set auto='$auto0', jam_on='$jamon0',menit_on='$meniton0',jam_off='$jamoff0',menit_off='$menitoff0' where id=0");
     $sql=mysql_query("update lampu set auto='$auto1', jam_on='$jamon1',menit_on='$meniton1',jam_off='$jamoff1',menit_off='$menitoff1' where id=1");
     $sql=mysql_query("update lampu set auto='$auto2', jam_on='$jamon2',menit_on='$meniton2',jam_off='$jamoff2',menit_off='$menitoff2' where id=2");
